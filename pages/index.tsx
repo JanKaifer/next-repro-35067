b28d63928b103caa1aa3548b256b1989258f6aec
Jan Kaifer <jan@kaifer.cz>
2023-01-12 15:00:34 +0100

    added repro

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,8 @@
+import Link from "next/link";
+
 /** Add your relevant code here for the issue to reproduce */
 export default function Home() {
-  return null
+  return <Link href=".">Link to self</Link>;
 }
+
+export const getStaticProps = null;
